3eaa57e9496580758c54126dc076933025f341bd
hor4tio <hor4tio.code@gmail.com>
2022-07-27 13:03:47 +0200

    first commit

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty, isNotEmpty, IsString } from "class-validator";
+
+export class AuthDto {
+
+	@IsEmail()
+	@IsNotEmpty()
+	email:		string;
+
+	@IsString()
+	@IsNotEmpty()
+	password:	string;
+}
---
+++ src/user/user.module.ts
@@ -0,0 +1,4 @@
+import { Module } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+@Module({})
+export class UserModule {}
